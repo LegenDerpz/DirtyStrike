3a2e451a9be537acc438fd441b200849761c4b4d
LegenDerpz <bcoydv@gmail.com>
2024-04-05 15:51:17 +0800

    PlayerMovement Update
    Updated PlayerMovement Script

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SocketIOClient;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class PlayerMovement : MonoBehaviour
 
     Vector2 movement;
     Vector2 mousePos;
+
 
     void Update()
     {
@@ -26,5 +28,26 @@ public class PlayerMovement : MonoBehaviour
         Vector2 lookDir = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
         rb.rotation = angle;
+
+        /*
+        if(socket.Connected){
+            SendMovementUpdate();
+        }
+        */
+    }
+
+    void SendMovementUpdate(){
+        PlayerMovementData data = new PlayerMovementData(rb.position, rb.velocity);
+        //socket.Emit("playerMovement", data);
+    }
+}
+
+public class PlayerMovementData{
+    public Vector2 position;
+    public Vector2 velocity;
+
+    public PlayerMovementData(Vector2 position, Vector2 velocity = default){
+        this.position = position;
+        this.velocity = velocity;
     }
 }
