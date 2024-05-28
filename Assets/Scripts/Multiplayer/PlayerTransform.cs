6ff929e91a075d8ecadef33ec6a5f383054b736d
Kent Jordan <kentaki.dev@gmail.com>
2024-05-28 13:22:03 +0800

    fix: player rotation and position

--- Assets/Scripts/Multiplayer/PlayerTransform.cs
+++ Assets/Scripts/Multiplayer/PlayerTransform.cs
@@ -2,6 +2,6 @@ public class PlayerTransform{
 
         public string username{get; set;}
         public PlayerPosition player_position {get; set;}
-        public PlayerRotation player_rotation {get; set;}
+        public float player_rotation {get; set;}
 
 }
